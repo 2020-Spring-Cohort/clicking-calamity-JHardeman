4f571943978d965b3ff0a58127dcdc04f95776e2
Jonathan <59838557+JHardeman@users.noreply.github.com>
2020-03-07 15:01:35 -0500

    Companion Tests written and passed

--- spec/js/ClickCounter.spec.js
+++ spec/js/ClickCounter.spec.js
@@ -4,7 +4,7 @@ describe('Clicking Calamity Tests:', () => {
         underTest = new ClickCount();
     })
     describe('countClick() records clicks and ClickCounter can give the clickCount', () => {
-        
+
         it('countClick() 1 time should result in a clickCount of 1.', () => {
             underTest.countClick();
             expect(underTest.clickCount).toBe(1);
@@ -15,22 +15,48 @@ describe('Clicking Calamity Tests:', () => {
             expect(underTest.clickCount).toBe(2);
         });
     });
-    
+
     describe('Clicking Companions:', () => {
         it("ClickCounter should have 0 when new.", () => {
             expect(underTest.getCompanionCount()).toBe(0);
         });
 
-        it('Click Companion available when clickCount equals 100', () => {
-            for(let i = 0; i<100; i++) {
+
+        it('1 Click Companion when ClickCounter equals 100', () => {
+            for (let i = 0; i < 100; i++) {
                 underTest.countClick()
             }
             underTest.buyCompanion()
-            expect(underTest.getCompanionCount()).toBe(0);
+            expect(underTest.getCompanionCount()).toBe(1);
+        });
+
+        it('ClickCounter can have multiple Companions', () => {
+            for (let i = 0; i < 220; i++) {
+                underTest.countClick()
+            }
+            underTest.buyCompanion()
+            underTest.buyCompanion()
+            expect(underTest.getCompanionCount()).toBe(2);
         });
 
 
+
+        it('Each Companion will increase its costs by 10% ', () => {
+            for (let i = 0; i < 110; i++) {
+                underTest.countClick();
+            }
+            underTest.buyCompanion()
+            underTest.buyCompanion()
+            expect(underTest.clickCount).toBe(10);
+            expect(underTest.companionCost).toBe(100);
+
+        });
+
     });
+
+
+    // Math.pow(1.2, compounderCount)
+
 
 
 
--- src/js/ClickCounter.js
+++ src/js/ClickCounter.js
@@ -3,22 +3,37 @@ class ClickCount {
         this.clickCount = 0;
         this.companionCount = 0;
         this.companionCost = 100;
+        // this.compounderCount = 0;
+        // this.compounderCost = 10;
     }
 
     countClick() {
         this.clickCount++;
     }
 
-    getCompanionCount(){
-        return 0;
+    displayclickCount(){
+        return this.clickCount;
     }
 
+    getCompanionCount(){
+        return this.companionCount;
+    }
+ 
 
     buyCompanion(){
-        if(this.clickCount >= this.companionCost)
-            return this.clickCount -= this.companionCost;
+        if (this.clickCount >= this.companionCost){
+            this.clickCount -= this.companionCost;
+            this.companionCount++;
+          
+        }
     }
 
+       companionCost(){
+        if(this.clickCount >= this.companionCost){
+            this.clickCount -= this.companionCost;
+            this.companionCost += Math.floor((this.companionCost * 1.1)); 
+        }
+            
+    }
 
-
-}
+  }
